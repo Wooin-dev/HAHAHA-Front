835b0265862f00628bda738cfefaf46b9d04ebbc
Hyeon woo Kim <wooin.dev@gmail.com>
2023-11-14 17:22:44 +0900

    [fix] 로그인 경고창 2회 동작 수정
    useEffect로 해당 코드 수정

--- src/Routes/ProtectedRoute.js
+++ src/Routes/ProtectedRoute.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useRecoilValue} from "recoil";
 import {isLoginSelector} from "../recoil/loginState";
 import {Navigate, Outlet} from "react-router-dom";
 
 function ProtectedRoute() {
     const isLogin = useRecoilValue(isLoginSelector);
-    if (!isLogin) alert("로그인이 필요한 페이지입니다.");
+    useEffect(() => {
+        if (!isLogin) alert("로그인이 필요한 페이지입니다.");
+        console.log("로그인 필요");
+    }, [])
     return isLogin ?
         <Outlet/> : <Navigate to={'/'}/>
 }
